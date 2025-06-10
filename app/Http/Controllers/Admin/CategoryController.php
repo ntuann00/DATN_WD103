3e42011008749e7ed20e77abe6e864962d957524
Quynhcuti <162964034+Quynhcuti@users.noreply.github.com>
2025-06-10 22:38:59 +0700

    create
    Thêm danh mục

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Models\Category;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller as BaseController;
 
 class CategoryController extends BaseController
@@ -17,10 +18,20 @@ class CategoryController extends BaseController
     }
      public function create()
     {
-      
+      return view('admin.categories.create');
     }
-     public function store()
-    {
+     public function store(Request $request)
+    {{
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+            'status' => 'required|in:active,inactive',
+        ]);
+
+        Category::create($validated);
+
+        return redirect()->route('categories.index')->with('success', 'Thêm mới thành công!');
+    }
     
     }
 
