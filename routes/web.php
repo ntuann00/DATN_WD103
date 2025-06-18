d65cc419d947d2d29e51501d3e19120acc252498
Quynhcuti <162964034+Quynhcuti@users.noreply.github.com>
2025-06-18 23:29:26 +0700

    edit/delete

--- routes/web.php
+++ routes/web.php
@@ -38,9 +38,9 @@ Route::delete('/categories/{id}', [CategoryController::class, 'destroy'])->name(
 Route::get('/attributes', [AttributeController::class, 'index'])->name('attributes.index');
 Route::get('/attributes/create', [AttributeController::class, 'create'])->name('attributes.create');
 Route::post('/attributes', [AttributeController::class, 'store'])->name('attributes.store');
-Route::get('/attributes/{id}', [AttributeController::class, 'show'])->name('attributes.show');
+
 Route::get('/attributes/{id}/edit', [AttributeController::class, 'edit'])->name('attributes.edit');
 Route::put('/attributes/{id}', [AttributeController::class, 'update'])->name('attributes.update');
-Route::delete('/attributes/{id}', [AttributeController::class, 'update'])->name('attributes.destroy');
+Route::delete('/attributes/{id}', [AttributeController::class, 'destroy'])->name('attributes.destroy');
 
 
